3eda2da563c09a192516728b7429c3865400dbf9
tsuyamakazuki <kazuki_tsuyama@bita.jp>
2023-05-25 18:49:07 +0900

    ・コンポーネント作成 ・v-bine使用

---
+++ src/components/Payment.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+const itemName1 = "Desk"
+const itemName2 = "Bike"
+
+const price1 = 40000
+const price2 = 20000
+
+const url1 = "https://gaming.logicool.co.jp/ja-jp/logitech-g-and-herman-miller.html"
+</script>
+
+<template>
+  <div class="container">
+    <h1>payment</h1>
+    <div class="payment">
+      <label>{{ itemName1 }}</label>
+      <label>{{ price1 }} yen</label>
+      <a v-bind:href="url1">bought at...</a>
+    </div>
+    <div class="payment">
+      <label>{{ itemName2 }}</label>
+      <label>{{ price2 }} yen</label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.payment {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  width: 400px;
+  background-color: #646cffaa;
+  margin-bottom: 8px;
+}
+label {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
